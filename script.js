a3049005e3a08634e874f05d435644b3eee79f47
OUSSAMA ECH-CHAREF <oussamacharef71@gmail.com>
2025-09-17 13:27:06 +0100

    git project

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+//CHAPITRE : Chapitre 1 – Variables, types de données et opérateurs
 let nombre1 = 10;
 let nombre2 = 5;
 let somme = nombre1 + nombre2;
 console.log("La somme est : " + somme);
+
+
+
+//CHAPITRE : Chapitre 2 – Fonctions simples
+function addition(a,b){
+    return a + b;
+}
+let somme1 = addition(8, 5);
+console.log("la sommeest : " + somme);
